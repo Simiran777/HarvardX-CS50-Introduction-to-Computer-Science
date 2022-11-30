655884323e798d53820b67adeb85f6c51454ffde
Simiran Mohapatra <63503814+Simiran987@users.noreply.github.com>
2022-11-30 23:16:34 +0530

    Add pset1

---
+++ pset1/mario-less/mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Variable for height
+    int height;
+    // Loop to keep asking height until 1<=height<=8
+    do
+    {
+        height = get_int("Height of the pyramid? ");
+    } while (height < 1 || height > 8);
+
+    // Loop for i no of rows
+    for (int i = 1; i <= height; i++)
+    {
+        // Loop for k no of spaces
+        for (int k = height - 1; k >= i; k--)
+        {
+            printf(" ");
+        }
+        // Loop for j no of columns
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
