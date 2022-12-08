71cc2ee269f943110b4a3bf0d3852665630047bd
Simiran Mohapatra <63503814+Simiran987@users.noreply.github.com>
2022-12-08 14:45:46 +0530

    Add cash

---
+++ pset1/cash/cash.c
@@ -0,0 +1,107 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    // TODO
+    int cents;
+    do
+    {
+        cents = get_int("Cents owed? ");
+    } while (cents < 0);
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+    // TODO
+    int quarters;
+    if (cents >= 25)
+    {
+        quarters = cents / 25;
+        return quarters;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_dimes(int cents)
+{
+    // TODO
+    int dimes;
+    if (cents >= 10)
+    {
+        dimes = cents / 10;
+        return dimes;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_nickels(int cents)
+{
+    // TODO
+    int nickels;
+    if (cents >= 5)
+    {
+        nickels = cents / 5;
+        return nickels;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int calculate_pennies(int cents)
+{
+    // TODO
+    int pennies;
+    if (cents >= 1)
+    {
+        pennies = cents / 1;
+        return pennies;
+    }
+    else
+    {
+        return 0;
+    }
+}
